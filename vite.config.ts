f2da407022f692b632943c5cbe1027efb496935c
Alan Dours <alan.dours@rootstrap.com>
2023-10-25 13:24:54 -0300

    Set entry file name

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "index.js",
+        manualChunks: undefined,
+      },
+    },
+  },
+  base: "./",
+});
