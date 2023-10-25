9b2bda88ceabaf54d3fee37db5c5e8e244a20a62
Alan Dours <alan.dours@rootstrap.com>
2023-10-25 12:42:06 -0300

    Render iframe and stylesheet

---
+++ src/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import ReactFrame from "react-frame-component";
+
+export const Frame = styled(ReactFrame)`
+  border: 0;
+  height: 24rem;
+  width: 16rem;
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+`;
