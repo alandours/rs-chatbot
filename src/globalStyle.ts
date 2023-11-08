0dd5e9067a1d272ae4d33a412032fcc53840dc64
Alan Dours <alan.dours@rootstrap.com>
2023-11-08 16:40:26 -0300

    Add fonts

--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -1,6 +1,21 @@
 import { createGlobalStyle } from "styled-components";
 
+import PoppinsRegular from "@/assets/fonts/Poppins/Poppins-Regular.ttf";
+import PoppinsMedium from "@/assets/fonts/Poppins/Poppins-Medium.ttf";
+
 export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Poppins';
+    src: url(${PoppinsRegular}) format('truetype');
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'Poppins';
+    src: url(${PoppinsMedium}) format('truetype');
+    font-weight: 500;
+  }
+
   * {
     box-sizing: border-box;
     margin: 0;
@@ -9,5 +24,6 @@ export const GlobalStyle = createGlobalStyle`
 
   body {
     overflow: hidden;
+    padding: 3.5rem;
   }
 `;
