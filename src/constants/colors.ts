ae81580cbcfdcb2cafa19307b343c3581f4e87e0
Alan Dours <alan.dours@rootstrap.com>
2023-11-08 16:42:06 -0300

    Add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export const CHATBOT_NAME = "Rootstrap Assistant";
+
+export const COLORS = {
+  BLACK: "#1B1B1B",
+  GREEN: "#009E3D",
+  GREY: {
+    LIGHTER: "#E2E2DE",
+    LIGHT: "#C0C0C0",
+    BASE: "#8D8D8D",
+    DARK: "#6F6F6E",
+    DARKER: "#212121",
+  },
+  RED: {
+    LIGHT: "#FDD8D8",
+    BASE: "#CB0B0B",
+    DARK: "#7E0707",
+  },
+  SHADOW: "rgba(0, 0, 0, 0.16)",
+  YELLOW: "#FFC83F",
+  WHITE: "#FFFFFF",
+};
