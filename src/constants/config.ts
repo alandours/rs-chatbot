81212d8134fb23756a0b78e25234446f163391ee
Alan Dours <alan.dours@rootstrap.com>
2024-01-12 16:15:18 -0300

    Update agent id

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -8,7 +8,7 @@ type Config = {
 
 export const CONFIG: Config = {
   API_URL: "https://api.chatbot-poc.rootstrap.net/api/v1",
-  AGENT_ID: 6,
+  AGENT_ID: 7,
   CHATBOT_NAME: "Ask Rootstrap AI",
   CHATBOT_USERNAME: "Rootbot",
   GOOGLE_RECAPTCHA_SITE_KEY: "6Lf_bDEpAAAAAI9PUblivbB8IKe1tbTqSP4HqzJi",
