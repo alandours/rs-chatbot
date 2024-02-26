d8852905113106cbfec6f7b3fab16f0a91d4115a
Dani Brosio <67912621+dbrosio3@users.noreply.github.com>
2024-02-26 17:10:10 -0300

    update recaptcha key (#29)

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -9,5 +9,5 @@ export const CONFIG: Config = {
   API_URL: "https://api.chatbot-poc.rootstrap.net/api/v1",
   CHATBOT_NAME: "Ask Rootstrap AI",
   CHATBOT_USERNAME: "Rootbot",
-  GOOGLE_RECAPTCHA_SITE_KEY: "6Lf_bDEpAAAAAI9PUblivbB8IKe1tbTqSP4HqzJi",
+  GOOGLE_RECAPTCHA_SITE_KEY: "6LeR-4ApAAAAAF2ZXUsGHBvvRn5m-NyJOJWhvfZf",
 };
